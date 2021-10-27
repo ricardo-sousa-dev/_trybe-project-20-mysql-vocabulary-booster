b128ab95b06a9ba0177e53e9548bf3c8891cf282
ricardo-sousa <rwmsousa@gmail.com>
2021-10-27 11:24:56 -0300

    requisito 9

---
+++ desafio9.sql
@@ -0,0 +1,17 @@
+-- 9 - Exibe todos as pessoas funcionárias que já realizaram algum pedido, mostrando também seu total de pedidos feitos
+-- Exiba todas as pessoas funcionárias que já realizaram algum pedido e o total dos pedidos feitos.
+-- Use o banco w3schools como referência
+-- Monte uma query que exiba 02 colunas:
+-- A primeira deve possuir o alias "Nome completo" e exibir o nome completo da pessoa funcionária (não se esqueça do espaço entre o nome e o sobrenome).
+-- A segunda deve possuir o alias "Total de pedidos" e exibir a quantidade total de pedidos feitos pela pessoa.
+-- Ordene seus resultados pelo total de pedidos em ordem crescente.
+
+SELECT 
+    CONCAT(e.FirstName, ' ', e.LastName) AS 'Nome completo',
+    COUNT(o.EmployeeID) AS 'Total de pedidos'
+FROM
+    w3schools.employees AS e
+        INNER JOIN
+    w3schools.orders AS o ON e.EmployeeID = o.EmployeeID
+GROUP BY 1
+ORDER BY 2;
